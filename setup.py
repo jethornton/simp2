e293dc1cefe09212ffb3017925c1e5f46e9cd3eb
John Thornton <jthornton@gnipsel.com>
2019-02-21 07:27:30 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simple2",
+    version="0.0.5",
+    author="John Doe",
+    author_email="<doe.john@example.com>",
+    description="Hello - An example to test install and docs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jethornton/hello",
+    download_url="https://github.com/jethornton/hello/tarball/master",
+    python_requires='>=3',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'gui_scripts': ['simp2=simp2.simp2:main',],
+    },
+)
+
---
+++ simp2/simp2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import platform
+
+print('Python {}'.format(platform.python_version()))
+
+"""
+install with 
+pip3 install -e .
+or 
+pip3 install .
+don't forget the trailing dot!
+"""
+
+import importme
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Simple')
+        self.show()
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
